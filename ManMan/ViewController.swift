1fdab3abb079a24538242aeb92d0aff2e61b6949
mavismwt <873498174@qq.com>
2018-12-05 20:14:47 +0800

    Initial Commit

---
+++ ManMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManMan
+//
+//  Created by Apple on 2018/12/5.
+//  Copyright © 2018年 Mavismwt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
